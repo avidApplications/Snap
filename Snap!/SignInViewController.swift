744607b2fe976c96adbb798f2f895bd92e362b98
Bradley Stovall <bradleystovall@Stovalls-MBA.local>
2017-05-05 18:32:26 -0800

    Firebase with first login screen

---
+++ Snap!/SignInViewController.swift
@@ -0,0 +1,51 @@
+//
+//  SignInViewController.swift
+//  Snap!
+//
+//  Created by Bradley Stovall on 5/5/17.
+//  Copyright © 2017 Bradley Stovall. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseAuth
+
+class SignInViewController: UIViewController {
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    // Button for initial screen sign in
+    @IBAction func goTapped(_ sender: Any) {
+        // Sign In
+        
+        FIRAuth.auth()?.signIn(withEmail: emailTextField.text!, password: passwordTextField.text!, completion: { (user, error) in
+            print("We tried to sign in")
+            if error != nil {
+                print("We have an error:\(String(describing: error))")
+                
+                // Create User
+                
+                FIRAuth.auth()?.createUser(withEmail: self.emailTextField.text!, password: self.passwordTextField.text!, completion: { (user, error) in
+                    print("We tried to create user")
+                    
+                    if error != nil {
+                        print("We have an error:\(String(describing: error))")
+                    } else {
+                        print("Created User Succesfully")
+                        self.performSegue(withIdentifier: "signInSegue", sender: nil)
+                    }
+                })
+            } else {
+                print("Signed in Succesfully")
+                self.performSegue(withIdentifier: "signInSegue", sender: nil)
+            }
+        })
+    }
+    
+    
+}
+
