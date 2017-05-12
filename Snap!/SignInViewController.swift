a8c34456033a5eb3be6a02960e1aad45eea2f2b9
Bradley Stovall <bradleystovall@Stovalls-MBA.local>
2017-05-12 10:38:18 -0800

    User creation

--- Snap!/SignInViewController.swift
+++ Snap!/SignInViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 import Firebase
 import FirebaseAuth
+import FirebaseDatabase
 
 class SignInViewController: UIViewController {
     
@@ -17,6 +18,8 @@ class SignInViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
     // Button for initial screen sign in
     @IBAction func goTapped(_ sender: Any) {
@@ -36,6 +39,11 @@ class SignInViewController: UIViewController {
                         print("We have an error:\(String(describing: error))")
                     } else {
                         print("Created User Succesfully")
+                // Saving users
+                        
+                FIRDatabase.database().reference().child("users").child(user!.uid).child("email").setValue(user!.email!)
+                        
+                        
                         self.performSegue(withIdentifier: "signInSegue", sender: nil)
                     }
                 })
